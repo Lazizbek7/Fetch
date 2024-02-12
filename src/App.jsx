53533f2eaf40c3e749178d0eb413472c37fa8973
Lazizbek <lazizbek19831910@gmail.com>
2024-02-12 21:04:19 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,34 @@ import User from './components/user'
 import People from './components/people'
 import Comments from './components/comments'
 import Products from './components/products'
+import ClipLoader from "react-spinners/ClipLoader";
+import { useState,useEffect } from 'react'
 function App() {
-
+  const [loading , setLoading] = useState(false)
+  useEffect(() =>{
+    setLoading(true)
+    setTimeout(() =>{
+      setLoading(false)
+    },7000)
+  } , [])
 
   return (
     <>
+    {
+      loading?
+      <ClipLoader className='Loader'
+        color={"#D0010B"}
+        loading={loading}
+        size={150}
+      />
+      :
+      <div>
       <Store/>
       <User/>
       <People/>
       <Comments/>
-      <Products/>
+      <Products/></div>
+    }
     </>
   )
 }
